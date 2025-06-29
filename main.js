2675c33b06c01deee5fd03627b9455c7597a04b1
conder <brenny.oshaughnessy@gmail.com>
2025-06-29 09:53:19 -0400

    added in pronouns

--- main.js
+++ main.js
@@ -8,21 +8,26 @@ const feedback = document.getElementById("feedback");
 const correctText = document.getElementById("correct");
 const tenses = ["INDICATIVE_PRESENT", "INDICATIVE_PRETERITE"];
 const verbs = ["hablar", "comer", "vivir", "ser", "estar", "tener", "hacer", "ir", "decir", "poder", "ver", "dar", "saber", "querer", "llegar", "pasar", "deber", "poner", "parecer", "quedar", "creer", "llevar", "dejar", "seguir", "encontrar", "llamar", "pensar", "salir", "volver", "tomar"];
+const pronouns = ["yo", "tú", "él/ella/usted", "nosotros", "vosotros", "ellos/ellas/ustedes"];
 var verb = "Verb";
 var tense = "Tense";
+var pronoun = "Pronoun";
+var number = 0;
 var total = 0;
 var correct = 0;
 
 function newVerb() {
    verb = verbs[Math.floor(Math.random() * verbs.length)];
    tense = tenses[Math.floor(Math.random() * tenses.length)];
-   verbText.textContent = verb + " - " + tense.toLocaleLowerCase();
+   number = Math.floor(Math.random() * pronouns.length);
+   pronoun = pronouns[number];
+   verbText.textContent = verb + " - " + tense.toLocaleLowerCase() + " - " + pronoun;
 }
 
 function checkAnswer(answer) {
    total++;
    console.log(answer);
-   var correctAnswer = getConjugation(verb, tense, 2);
+   var correctAnswer = getConjugation(verb, tense, number);
    if (answer == correctAnswer) {
       correct++;
       feedback.textContent = "Correct!";
